c047fe6a8a0a8cffc4e9a014254d601db5e19fa5
Maurice Miema <mauricemiema90@gmail.com>
2024-12-20 13:46:47 +0100

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+function App() {
+  return (
+    <>
+      <div className="flex justify-center">
+        <h1 className="text-2xl font-semibold text-red-500 pt-2">Hello World</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
