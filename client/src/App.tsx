03f060f0aa82ac8c395a36d38a96133ed50891f8
Maurice Miema <mauricemiema90@gmail.com>
2024-12-30 20:47:52 +0100

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-4xl text-red-500">L'Application est en cours de Developpement</h1>
+    </div>
+  )
+}
+
+export default App
